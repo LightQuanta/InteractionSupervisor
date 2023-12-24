634d8f5b9a9fcf1c79687a8527bf8fa6d59e1bb2
Light_Quanta <zch22248@foxmail.com>
2023-12-24 11:53:48 +0800

    首次提交

---
+++ src/main/kotlin/tech/lq0/interactionsupervisor/Main.kt
@@ -0,0 +1,14 @@
+package tech.lq0.interactionsupervisor
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class Main : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}

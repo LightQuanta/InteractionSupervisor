0a650c5d6c9b06d3a5fdfb6799eeb06cd40689c9
Light_Quanta <zch22248@foxmail.com>
2023-12-26 00:39:58 +0800

    实现延迟消息的shadow ban

--- src/main/kotlin/tech/lq0/interactionsupervisor/event/ChatHandler.kt
+++ src/main/kotlin/tech/lq0/interactionsupervisor/event/ChatHandler.kt
@@ -17,27 +17,51 @@ object ChatHandler : Listener {
     @EventHandler
     fun onChat(event: AsyncPlayerChatEvent) {
         val message = event.message
-        val player = event.player
+        val sender = event.player
 
+        // 启用聊天延迟时
         if (chatDelayEnabled) {
             GlobalScope.launch {
                 val uuid = UUID.randomUUID()
-                val chat = ChatMessage(player, message)
+                val chat = ChatMessage(sender, message)
                 delayedChat[uuid] = chat
                 log.info(
                     "Delayed Chat: " + chatFormat
-                        .replace("%DISPLAY_NAME%", player.displayName)
-                        .replace("%NAME%", player.name)
+                        .replace("%DISPLAY_NAME%", sender.displayName)
+                        .replace("%NAME%", sender.name)
                         .replace("%MESSAGE%", message)
                 )
-                svr.onlinePlayers.filter { canBypassDelay(it, chat) }.forEach {
-                    it.sendMessage(
-                        chatFormat
-                            .replace("%DISPLAY_NAME%", player.displayName)
-                            .replace("%NAME%", player.name)
+                if (message.isSensitive()) {
+                    log.warning("Sensitive words send by ${event.player.name} : $message")
+                    sender.shadowBan()
+                }
+
+                // 如果发送者能跳过消息延迟，则立刻向发送者发送消息
+                if (canBypassDelay(sender, chat)) {
+                    sender.sendMessage(
+                        chatFormat.replace("%DISPLAY_NAME%", sender.displayName)
+                            .replace("%NAME%", sender.name)
                             .replace("%MESSAGE%", message)
                     )
                 }
+
+                // 向其余能跳过消息延迟的玩家立刻发送消息
+                if (!message.isSensitive() && !sender.shadowBanned()) {
+                    svr.onlinePlayers
+                        .filter { it.uniqueId != sender.uniqueId }
+                        .filter { canBypassDelay(it, chat) }
+                        .forEach {
+                            log.info("delayed chat bypassed by ${it.name} : $message")
+                            it.sendMessage(
+                                chatFormat
+                                    .replace("%DISPLAY_NAME%", sender.displayName)
+                                    .replace("%NAME%", sender.name)
+                                    .replace("%MESSAGE%", message)
+                            )
+                        }
+                }
+
+                // 延迟后发送消息
                 delay(chatDelay * 1000L)
                 sendDelayedChat(uuid)
             }
@@ -45,31 +69,35 @@ object ChatHandler : Listener {
             return
         }
 
-        if (player.shadowBanned()) {
-            event.isCancelled = true
-            player.sendMessage(
+        // 未开启消息延迟情况
+        // 针对shadow ban玩家发送假消息，并拦截事件
+        if (sender.shadowBanned()) {
+            sender.sendMessage(
                 chatFormat
-                    .replace("%DISPLAY_NAME%", player.displayName)
-                    .replace("%NAME%", player.name)
+                    .replace("%DISPLAY_NAME%", sender.displayName)
+                    .replace("%NAME%", sender.name)
                     .replace("%MESSAGE%", message)
             )
+            event.isCancelled = true
             return
         }
 
+        // 针对敏感消息，对发送者shadow ban，并拦截事件
         if (message.isSensitive()) {
             log.warning("Sensitive words send by ${event.player.name} : $message")
-            player.shadowBan()
+            sender.shadowBan()
             // shadow ban
-            player.sendMessage(
+            sender.sendMessage(
                 chatFormat
-                    .replace("%DISPLAY_NAME%", player.displayName)
-                    .replace("%NAME%", player.name)
+                    .replace("%DISPLAY_NAME%", sender.displayName)
+                    .replace("%NAME%", sender.name)
                     .replace("%MESSAGE%", message)
             )
             event.isCancelled = true
         }
     }
 
+    // 清空延迟发送消息列表，并返回清空数量
     fun clearDelayedMessage(): Int = delayedChat.size.also { delayedChat.clear() }
 
     private fun canBypassDelay(player: Player, chat: ChatMessage): Boolean {
@@ -79,27 +107,34 @@ object ChatHandler : Listener {
     }
 
     private fun sendDelayedChat(uuid: UUID) {
-        if (delayedChat.contains(uuid)) {
-            val chatInfo = delayedChat[uuid]!!
-            val player = chatInfo.player
-            val message = chatInfo.message
-            if (player.shadowBanned() && !senderReceiveImmediately) {
-                player.sendMessage(
+        val chatInfo = delayedChat[uuid] ?: return
+        val sender = chatInfo.player
+        val message = chatInfo.message
+
+        // 对发送者特判，确保发送者能收到自己的延迟消息，防止因为shadow ban和敏感词导致该消息被跳过
+        if (!canBypassDelay(sender, chatInfo)) {
+            log.info("Delayed message to sender by ${sender.name} : $message")
+            sender.sendMessage(
+                chatFormat
+                    .replace("%DISPLAY_NAME%", sender.displayName)
+                    .replace("%NAME%", sender.name)
+                    .replace("%MESSAGE%", message)
+            )
+        }
+
+        // 向其余没有跳过延迟的玩家发送聊天消息
+        if (sender.shadowBanned() || message.isSensitive()) return
+        svr.onlinePlayers
+            .filter { it.uniqueId != sender.uniqueId }
+            .filter { !canBypassDelay(it, chatInfo) }
+            .forEach {
+                log.info("Delayed message to ${it.name} by ${sender.name} : $message")
+                it.sendMessage(
                     chatFormat
-                        .replace("%DISPLAY_NAME%", player.displayName)
-                        .replace("%NAME%", player.name)
+                        .replace("%DISPLAY_NAME%", sender.displayName)
+                        .replace("%NAME%", sender.name)
                         .replace("%MESSAGE%", message)
                 )
-            } else {
-                svr.onlinePlayers.filter { !canBypassDelay(it, chatInfo) }.forEach {
-                    it.sendMessage(
-                        chatFormat
-                            .replace("%DISPLAY_NAME%", player.displayName)
-                            .replace("%NAME%", player.name)
-                            .replace("%MESSAGE%", message)
-                    )
-                }
             }
-        }
     }
 }
